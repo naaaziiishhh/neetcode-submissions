4e2663796afe95421e1fa66be56ee2ab0bed58ba
naaaziiishhh <dev.nazish@gmail.com>
2026-09-15 20:40:16 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if(!seen.add(num)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+// O(n) Time and O(n) space

bf7a3c51f3b7a51f5633afd896c91cd8eb176486
naaaziiishhh <dev.nazish@gmail.com>
2026-09-15 20:57:12 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] schar = s.toCharArray();
+        char[] tchar = t.toCharArray();
+
+        Arrays.sort(schar);
+        Arrays.sort(tchar);
+
+        String ssorted = new String(schar);
+        String tsorted = new String(tchar);
+
+        return ssorted.equals(tsorted);
+    }
+}
+
+// Brute Force 
